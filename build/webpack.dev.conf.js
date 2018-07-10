ee7ba6fb353a3524e84519612b2dbc8273b91920
qh-template <wadejs_wxtest1@sina.com>
2018-07-10 16:14:53 +0800

    test

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -48,7 +48,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env')
+      'process.env': require(`../${folder}/config/dev.env`)
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
